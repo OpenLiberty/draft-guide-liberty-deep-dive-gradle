f2518250c1b34275ee8873a2a7a82f452431e528
Gilbert Kwan <gkwan@ca.ibm.com>
2023-03-30 18:00:28 -0400

    Update dockerImageTest.sh

--- scripts/dockerImageTest.sh
+++ scripts/dockerImageTest.sh
@@ -13,22 +13,22 @@ echo "Testing daily Docker image"
 
 export RUNTIMEURL="apply plugin: 'liberty'\nliberty {\n    install {\n        runtimeUrl='https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER'\n    }\n}"
 
-echo "$RUNTIMEURL" >> module-jwt/build.gradle
+echo -e $RUNTIMEURL" >> module-jwt/build.gradle
 cat module-jwt/build.gradle
 
-echo "$RUNTIMEURL" >> module-getting-started/build.gradle
+echo -e "$RUNTIMEURL" >> module-getting-started/build.gradle
 cat module-getting-started/build.gradle
 
-echo "$RUNTIMEURL" >> system/build.gradle
+echo -e "$RUNTIMEURL" >> system/build.gradle
 cat system/build.gradle
 
-echo "$RUNTIMEURL" >> module-config/build.gradle
+echo -e "$RUNTIMEURL" >> module-config/build.gradle
 cat module-config/build.gradle
 
-echo "$RUNTIMEURL" >> module-securing/build.gradle
+echo -e "$RUNTIMEURL" >> module-securing/build.gradle
 cat module-securing/build.gradle
 
-echo  "$RUNTIMEURL" >> module-openapi/build.gradle
+echo -e "$RUNTIMEURL" >> module-openapi/build.gradle
 cat module-openapi/build.gradle
 
 sed -i "s;FROM icr.io/appcafe/open-liberty:full-java11-openj9-ubi;FROM openliberty/daily:latest;g" module-kubernetes/Containerfile
