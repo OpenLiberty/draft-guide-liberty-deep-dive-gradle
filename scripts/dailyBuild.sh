6bc2343d31467c997600ca21cfb60d218687aac7
Gilbert Kwan <gkwan@ca.ibm.com>
2023-04-03 14:54:45 -0400

    Update dailyBuild.sh

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -13,22 +13,23 @@ echo "Testing daily build image"
 
 export RUNTIMEURL="\nliberty {\n    install {\n        runtimeUrl='https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER'\n    }\n}"
 
-echo -e "$RUNTIMEURL" >> module-jwt/build.gradle
+#echo -e "$RUNTIMEURL" >> module-jwt/build.gradle
+sed -i "s;clean.dependsOn;$RUNTIMEURL\nclean.dependsOn;g" module-jwt/build.gradle module-getting-started/build.gradle system/build.gradle module-config/build.gradle module-securing/build.gradle module-openapi/build.gradle
 cat module-jwt/build.gradle
 
-echo -e "$RUNTIMEURL" >> module-getting-started/build.gradle
+#echo -e "$RUNTIMEURL" >> module-getting-started/build.gradle
 cat module-getting-started/build.gradle
 
-echo -e "$RUNTIMEURL" >> system/build.gradle
+#echo -e "$RUNTIMEURL" >> system/build.gradle
 cat system/build.gradle
 
-echo -e "$RUNTIMEURL" >> module-config/build.gradle
+#echo -e "$RUNTIMEURL" >> module-config/build.gradle
 cat module-config/build.gradle
 
-echo -e "$RUNTIMEURL" >> module-securing/build.gradle
+#echo -e "$RUNTIMEURL" >> module-securing/build.gradle
 cat module-securing/build.gradle
 
-echo -e "$RUNTIMEURL" >> module-openapi/build.gradle
+#echo -e "$RUNTIMEURL" >> module-openapi/build.gradle
 cat module-openapi/build.gradle
 
 sed -i "s;FROM icr.io/appcafe/open-liberty:full-java11-openj9-ubi;FROM $DOCKER_USERNAME/olguides:$BUILD;g" module-kubernetes/Containerfile
