d409f7bc7c1d1c52a97ea196f19cff54b8deb3ba
Gilbert Kwan <gkwan@ca.ibm.com>
2023-04-03 17:10:30 -0400

    Update dockerImageTest.sh

--- scripts/dockerImageTest.sh
+++ scripts/dockerImageTest.sh
@@ -11,24 +11,20 @@ done
 echo "Testing daily Docker image"
 
 
-export RUNTIMEURL="apply plugin: 'liberty'\nliberty {\n    install {\n        runtimeUrl='https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER'\n    }\n}"
+export RUNTIMEURL="liberty {\n    install {\n        runtimeUrl='https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER'\n    }\n}\n"
 
-echo -e "$RUNTIMEURL" >> module-jwt/build.gradle
+sed -i "s;clean.dependsOn;$RUNTIMEURL\nclean.dependsOn;g" module-jwt/build.gradle module-getting-started/build.gradle system/build.gradle module-config/build.gradle module-securing/build.gradle module-openapi/build.gradle
+echo "=== module-jwt/build.gradle ==="
 cat module-jwt/build.gradle
-
-echo -e "$RUNTIMEURL" >> module-getting-started/build.gradle
+echo "=== module-getting-started/build.gradle ==="
 cat module-getting-started/build.gradle
-
-echo -e "$RUNTIMEURL" >> system/build.gradle
+echo "=== system/build.gradle ==="
 cat system/build.gradle
-
-echo -e "$RUNTIMEURL" >> module-config/build.gradle
+echo "=== module-config/build.gradle ==="
 cat module-config/build.gradle
-
-echo -e "$RUNTIMEURL" >> module-securing/build.gradle
+echo "=== module-securing/build.gradle ==="
 cat module-securing/build.gradle
-
-echo -e "$RUNTIMEURL" >> module-openapi/build.gradle
+echo "=== module-openapi/build.gradle ==="
 cat module-openapi/build.gradle
 
 sed -i "s;FROM icr.io/appcafe/open-liberty:full-java11-openj9-ubi;FROM openliberty/daily:latest;g" module-kubernetes/Containerfile
