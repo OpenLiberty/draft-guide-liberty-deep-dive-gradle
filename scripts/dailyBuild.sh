dc43e5874a42a460c192caa2e69a74cafe69f98c
Gilbert Kwan <gkwan@ca.ibm.com>
2024-07-03 09:25:19 -0400

    Update dailyBuild.sh

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-while getopts t:d:j: flag; do
+while getopts t:d:b:u:j: flag; do
     case "${flag}" in
     t) DATE="${OPTARG}" ;;
     d) DRIVER="${OPTARG}" ;;
+    b) BUILD="${OPTARG}";;
+    u) DOCKER_USERNAME="${OPTARG}";;
     j) JDK_LEVEL="${OPTARG}" ;;
     *) echo "Invalid option" ;;
     esac
